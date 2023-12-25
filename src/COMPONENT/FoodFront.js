1cb0725682e9a01274d34e95b87a16c03711549d
shajith10 <dummymail071045@gmail.com>
2023-12-25 13:15:25 +0530

    first commit

--- src/COMPONENT/FoodFront.js
+++ src/COMPONENT/FoodFront.js
@@ -29,7 +29,7 @@ function FoodFront() {
     }
 
     const [datas, setdatas] = useState(initialValue)
-    const randomSubtraction = 5000;
+    const randomSubtraction = 50000;
 
 
     const cuisines = [
